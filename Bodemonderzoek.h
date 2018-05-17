888045788c7106ad2b283e5f28dcb05de7665ea3
Aaldert <daan@compagner.com>
2018-05-17 09:10:26 +0200

    Bodem Shader added !Note: Add Shader File And Models to Runtime!

--- Bodemonderzoek.h
+++ Bodemonderzoek.h
@@ -2,21 +2,20 @@
 #pragma once
 #include "Uniforms.h"
 
-#include <VrLib\Application.h>
-#include <VrLib\tien\Tien.h>
-#include <VrLib\HtcVive.h>
-#include <VrLib\Device.h>
-#include <glm\glm.hpp>
+#include <VrLib/Application.h>
+#include <VrLib/tien/Tien.h>
+#include <VrLib/HtcVive.h>
+#include <VrLib/Device.h>
+#include <glm/glm.hpp>
 
-class Bodemonderzoek : public vrlib::Application{
+class Bodemonderzoek : public vrlib::Application
+{
 	vrlib::Vive vive;
 	vrlib::tien::Tien Engine;
 
 public:
 	Bodemonderzoek();
 	~Bodemonderzoek();
-
-	vrlib::Texture* gridTexture;
 
 	virtual void init() override;
 	virtual void draw(const glm::mat4 &projectionMatrix, const glm::mat4 &modelViewMatrix) override;
@@ -26,4 +25,22 @@ public:
 	void loadScene();
 private:
 	bool showDebug;
+
+	vrlib::Texture* mixmap_texture;
+	vrlib::Texture* texture0;
+	vrlib::Texture* texture1;
+
+	enum class Uniforms
+	{
+		modelMatrix,
+		projectionMatrix,
+		viewMatrix,
+		s_mixmap,
+		s_texture0,
+		s_texture1,
+		diffuseColor,
+		textureFactor
+	};
+
+	vrlib::gl::Shader<Uniforms>* shader;
 };
